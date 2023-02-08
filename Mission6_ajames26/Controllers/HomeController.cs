0cd66f12abfe035aae87da3cb17857be5e9e6c4c
ajames <ajames1447@gmail.com>
2023-02-08 13:48:58 -0700

    Add project files.

---
+++ Mission6_ajames26/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6_ajames26.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6_ajames26.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Bacon()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EnterMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnterMovie(Movie movie)
+        {
+            Console.WriteLine($"Entering movie {movie.Title}");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission6_ajames26/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission6_ajames26.Models
+{
+    public class Movie
+    {
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public uint Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        [MaxLength(5)]
+        public string Rating { get; set; }
+        public bool? Edited { get; set; } = false;
+        public string? LentTo { get; set; }
+        [MaxLength(25)]
+        public string? Notes { get; set; }
+    }
+}
